36870d17dec1144f0909c146ee2d837d3df887ca
Jioh Jeon <69615755+gitgio99@users.noreply.github.com>
2025-07-21 16:32:53 +0900

    add function RTC (CMD time0x03)
    change CRC16/ led_font 분리 / rpi4d 서버시간 받아오기 위한 구조 재조정

---
+++ Core/Src/main.c
@@ -0,0 +1,490 @@
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * @file           : main.c
+  * @brief          : Main program body
+  ******************************************************************************
+  * @attention
+  *
+  * Copyright (c) 2025 STMicroelectronics.
+  * All rights reserved.
+  *
+  * This software is licensed under terms that can be found in the LICENSE file
+  * in the root directory of this software component.
+  * If no LICENSE file comes with this software, it is provided AS-IS.
+  *
+  ******************************************************************************
+  */
+/* USER CODE END Header */
+/* Includes ------------------------------------------------------------------*/
+#include "main.h"
+
+/* Private includes ----------------------------------------------------------*/
+/* USER CODE BEGIN Includes */
+#include <stdio.h>
+
+/* USER CODE END Includes */
+
+/* Private typedef -----------------------------------------------------------*/
+/* USER CODE BEGIN PTD */
+
+/* USER CODE END PTD */
+
+/* Private define ------------------------------------------------------------*/
+/* USER CODE BEGIN PD */
+
+/* USER CODE END PD */
+
+/* Private macro -------------------------------------------------------------*/
+/* USER CODE BEGIN PM */
+
+/* USER CODE END PM */
+
+/* Private variables ---------------------------------------------------------*/
+RTC_HandleTypeDef hrtc;
+
+TIM_HandleTypeDef htim3;
+DMA_HandleTypeDef hdma_tim3_ch4_up;
+
+UART_HandleTypeDef huart1;
+
+/* USER CODE BEGIN PV */
+uint8_t led_enabled = 0;
+/* USER CODE END PV */
+
+/* Private function prototypes -----------------------------------------------*/
+void SystemClock_Config(void);
+static void MX_GPIO_Init(void);
+static void MX_DMA_Init(void);
+static void MX_RTC_Init(void);
+static void MX_TIM3_Init(void);
+static void MX_USART1_UART_Init(void);
+/* USER CODE BEGIN PFP */
+
+/* USER CODE END PFP */
+
+/* Private user code ---------------------------------------------------------*/
+/* USER CODE BEGIN 0 */
+
+/* USER CODE END 0 */
+
+/**
+  * @brief  The application entry point.
+  * @retval int
+  */
+int main(void)
+{
+
+  /* USER CODE BEGIN 1 */
+
+  /* USER CODE END 1 */
+
+  /* MCU Configuration--------------------------------------------------------*/
+
+  /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
+  HAL_Init();
+
+  /* USER CODE BEGIN Init */
+
+  /* USER CODE END Init */
+
+  /* Configure the system clock */
+  SystemClock_Config();
+
+  /* USER CODE BEGIN SysInit */
+
+  /* USER CODE END SysInit */
+
+  /* Initialize all configured peripherals */
+  MX_GPIO_Init();
+  MX_DMA_Init();
+  MX_RTC_Init();
+  MX_TIM3_Init();
+  MX_USART1_UART_Init();
+  /* USER CODE BEGIN 2 */
+//  // 픽셀 테스트
+//      clearBuffer();  // 프레임 버퍼 초기화
+//
+//      // 초록색 픽셀 찍기
+//      setPixel(4, 12, 1, 0, 0);   // (12, 4)
+//      setPixel(17, 24, 1, 1, 0);  // (24, 17)
+
+	char *msg = "UART Bitmask FSM Receiver Start\r\n";
+	HAL_UART_Transmit(&huart1, (uint8_t*)msg, strlen(msg), HAL_MAX_DELAY);
+	HAL_UART_Receive_IT(&huart1, &rx_byte, 1);  // Start interrupt receive
+
+
+  //HAL_UART_Receive_IT(&huart1, &rx_byte,1);
+  //drawGo();
+  //HUB75_UpdateScreen();
+  //HAL_Delay(2000);  // 2초 보여주기
+//	clearBuffer();
+
+  /* USER CODE END 2 */
+
+  /* Infinite loop */
+  /* USER CODE BEGIN WHILE */
+  RTC_TimeTypeDef sTime;
+  RTC_DateTypeDef sDate;
+  char temp[100];
+  const char* ampm;
+
+  uint8_t last_minute = 0xFF;  // 초기값: 무조건 첫 프레임 표시
+  uint32_t last_tick = 0;
+
+
+  while (1)
+  {
+
+
+    /* USER CODE END WHILE */
+
+    /* USER CODE BEGIN 3 */
+
+	HUB75_UpdateScreen();
+	if (HAL_GetTick() - last_tick >= 1000) {
+		last_tick = HAL_GetTick();
+
+		HAL_RTC_GetTime(&hrtc, &sTime, RTC_FORMAT_BIN);
+		HAL_RTC_GetDate(&hrtc, &sDate, RTC_FORMAT_BIN);
+
+		const char* ampm = (sTime.TimeFormat == RTC_HOURFORMAT12_AM) ? "AM" : "PM";
+
+		if (sTime.Minutes != last_minute) {
+			last_minute = sTime.Minutes;
+
+			clearBuffer();
+			if (led_enabled) {
+				drawStopNow();  // ON 상태일 때 STOP NOW 메시지
+			} else {
+				RTC_HandleTypeDef hrtc;
+				drawClockTime(sTime.Hours, sTime.Minutes, ampm);  // OFF 상태에서도 시계는 표시
+			}
+		}
+
+		sprintf(temp, "\r\n20%02x-%02x-%02x %s %02x:%02x:%02x",
+				sDate.Year, sDate.Month, sDate.Date,
+				ampm, sTime.Hours, sTime.Minutes, sTime.Seconds);
+		HAL_UART_Transmit(&huart1, (uint8_t*)temp, strlen(temp), HAL_MAX_DELAY);
+	}
+
+  /* USER CODE END 3 */
+  }
+}
+
+/**
+  * @brief System Clock Configuration
+  * @retval None
+  */
+void SystemClock_Config(void)
+{
+  RCC_OscInitTypeDef RCC_OscInitStruct = {0};
+  RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
+
+  /** Configure the main internal regulator output voltage
+  */
+  __HAL_RCC_PWR_CLK_ENABLE();
+  __HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE2);
+
+  /** Initializes the RCC Oscillators according to the specified parameters
+  * in the RCC_OscInitTypeDef structure.
+  */
+  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI|RCC_OSCILLATORTYPE_LSE;
+  RCC_OscInitStruct.LSEState = RCC_LSE_ON;
+  RCC_OscInitStruct.HSIState = RCC_HSI_ON;
+  RCC_OscInitStruct.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
+  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
+  RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSI;
+  RCC_OscInitStruct.PLL.PLLM = 16;
+  RCC_OscInitStruct.PLL.PLLN = 336;
+  RCC_OscInitStruct.PLL.PLLP = RCC_PLLP_DIV4;
+  RCC_OscInitStruct.PLL.PLLQ = 7;
+  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
+  {
+    Error_Handler();
+  }
+
+  /** Initializes the CPU, AHB and APB buses clocks
+  */
+  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
+                              |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
+  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
+  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV2;
+  RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
+
+  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_2) != HAL_OK)
+  {
+    Error_Handler();
+  }
+}
+
+/**
+  * @brief RTC Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_RTC_Init(void)
+{
+
+  /* USER CODE BEGIN RTC_Init 0 */
+
+  /* USER CODE END RTC_Init 0 */
+
+  RTC_TimeTypeDef sTime = {0};
+  RTC_DateTypeDef sDate = {0};
+
+  /* USER CODE BEGIN RTC_Init 1 */
+
+  /* USER CODE END RTC_Init 1 */
+
+  /** Initialize RTC Only
+  */
+  hrtc.Instance = RTC;
+  hrtc.Init.HourFormat = RTC_HOURFORMAT_12;
+  hrtc.Init.AsynchPrediv = 127;
+  hrtc.Init.SynchPrediv = 255;
+  hrtc.Init.OutPut = RTC_OUTPUT_DISABLE;
+  hrtc.Init.OutPutPolarity = RTC_OUTPUT_POLARITY_HIGH;
+  hrtc.Init.OutPutType = RTC_OUTPUT_TYPE_OPENDRAIN;
+  if (HAL_RTC_Init(&hrtc) != HAL_OK)
+  {
+    Error_Handler();
+  }
+
+  /* USER CODE BEGIN Check_RTC_BKUP */
+
+  /* USER CODE END Check_RTC_BKUP */
+
+  /** Initialize RTC and set the Time and Date
+  */
+  sTime.Hours = 1;
+  sTime.Minutes = 0;
+  sTime.Seconds = 0;
+  sTime.TimeFormat = RTC_HOURFORMAT12_AM;
+  sTime.DayLightSaving = RTC_DAYLIGHTSAVING_NONE;
+  sTime.StoreOperation = RTC_STOREOPERATION_RESET;
+  if (HAL_RTC_SetTime(&hrtc, &sTime, RTC_FORMAT_BIN) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  sDate.WeekDay = RTC_WEEKDAY_MONDAY;
+  sDate.Month = RTC_MONTH_JANUARY;
+  sDate.Date = 1;
+  sDate.Year = 0;
+
+  if (HAL_RTC_SetDate(&hrtc, &sDate, RTC_FORMAT_BIN) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /* USER CODE BEGIN RTC_Init 2 */
+
+  /* USER CODE END RTC_Init 2 */
+
+}
+
+/**
+  * @brief TIM3 Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_TIM3_Init(void)
+{
+
+  /* USER CODE BEGIN TIM3_Init 0 */
+
+  /* USER CODE END TIM3_Init 0 */
+
+  TIM_ClockConfigTypeDef sClockSourceConfig = {0};
+  TIM_MasterConfigTypeDef sMasterConfig = {0};
+
+  /* USER CODE BEGIN TIM3_Init 1 */
+
+  /* USER CODE END TIM3_Init 1 */
+  htim3.Instance = TIM3;
+  htim3.Init.Prescaler = 83;
+  htim3.Init.CounterMode = TIM_COUNTERMODE_UP;
+  htim3.Init.Period = 99;
+  htim3.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
+  htim3.Init.AutoReloadPreload = TIM_AUTORELOAD_PRELOAD_DISABLE;
+  if (HAL_TIM_Base_Init(&htim3) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  sClockSourceConfig.ClockSource = TIM_CLOCKSOURCE_INTERNAL;
+  if (HAL_TIM_ConfigClockSource(&htim3, &sClockSourceConfig) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  sMasterConfig.MasterOutputTrigger = TIM_TRGO_RESET;
+  sMasterConfig.MasterSlaveMode = TIM_MASTERSLAVEMODE_DISABLE;
+  if (HAL_TIMEx_MasterConfigSynchronization(&htim3, &sMasterConfig) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /* USER CODE BEGIN TIM3_Init 2 */
+
+  /* USER CODE END TIM3_Init 2 */
+
+}
+
+/**
+  * @brief USART1 Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_USART1_UART_Init(void)
+{
+
+  /* USER CODE BEGIN USART1_Init 0 */
+
+  /* USER CODE END USART1_Init 0 */
+
+  /* USER CODE BEGIN USART1_Init 1 */
+
+  /* USER CODE END USART1_Init 1 */
+  huart1.Instance = USART1;
+  huart1.Init.BaudRate = 115200;
+  huart1.Init.WordLength = UART_WORDLENGTH_8B;
+  huart1.Init.StopBits = UART_STOPBITS_1;
+  huart1.Init.Parity = UART_PARITY_NONE;
+  huart1.Init.Mode = UART_MODE_TX_RX;
+  huart1.Init.HwFlowCtl = UART_HWCONTROL_NONE;
+  huart1.Init.OverSampling = UART_OVERSAMPLING_16;
+  if (HAL_UART_Init(&huart1) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /* USER CODE BEGIN USART1_Init 2 */
+
+  /* USER CODE END USART1_Init 2 */
+
+}
+
+/**
+  * Enable DMA controller clock
+  */
+static void MX_DMA_Init(void)
+{
+
+  /* DMA controller clock enable */
+  __HAL_RCC_DMA1_CLK_ENABLE();
+
+  /* DMA interrupt init */
+  /* DMA1_Stream2_IRQn interrupt configuration */
+  HAL_NVIC_SetPriority(DMA1_Stream2_IRQn, 0, 0);
+  HAL_NVIC_EnableIRQ(DMA1_Stream2_IRQn);
+
+}
+
+/**
+  * @brief GPIO Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_GPIO_Init(void)
+{
+  GPIO_InitTypeDef GPIO_InitStruct = {0};
+  /* USER CODE BEGIN MX_GPIO_Init_1 */
+
+  /* USER CODE END MX_GPIO_Init_1 */
+
+  /* GPIO Ports Clock Enable */
+  __HAL_RCC_GPIOC_CLK_ENABLE();
+  __HAL_RCC_GPIOH_CLK_ENABLE();
+  __HAL_RCC_GPIOA_CLK_ENABLE();
+  __HAL_RCC_GPIOB_CLK_ENABLE();
+
+  /*Configure GPIO pin Output Level */
+  HAL_GPIO_WritePin(GPIOC, A_Pin|B_Pin|C_Pin|D_Pin, GPIO_PIN_RESET);
+
+  /*Configure GPIO pin Output Level */
+  HAL_GPIO_WritePin(CLK_GPIO_Port, CLK_Pin, GPIO_PIN_SET);
+
+  /*Configure GPIO pin Output Level */
+  HAL_GPIO_WritePin(LD2_GPIO_Port, LD2_Pin, GPIO_PIN_RESET);
+
+  /*Configure GPIO pin Output Level */
+  HAL_GPIO_WritePin(GPIOB, R1_Pin|G1_Pin|B1_Pin|R2_Pin
+                          |G2_Pin|B2_Pin|LAT_Pin, GPIO_PIN_RESET);
+
+  /*Configure GPIO pin Output Level */
+  HAL_GPIO_WritePin(OE_GPIO_Port, OE_Pin, GPIO_PIN_SET);
+
+  /*Configure GPIO pins : A_Pin B_Pin C_Pin D_Pin */
+  GPIO_InitStruct.Pin = A_Pin|B_Pin|C_Pin|D_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_PULLUP;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_HIGH;
+  HAL_GPIO_Init(GPIOC, &GPIO_InitStruct);
+
+  /*Configure GPIO pin : CLK_Pin */
+  GPIO_InitStruct.Pin = CLK_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_PULLUP;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_HIGH;
+  HAL_GPIO_Init(CLK_GPIO_Port, &GPIO_InitStruct);
+
+  /*Configure GPIO pin : LD2_Pin */
+  GPIO_InitStruct.Pin = LD2_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  HAL_GPIO_Init(LD2_GPIO_Port, &GPIO_InitStruct);
+
+  /*Configure GPIO pins : R1_Pin G1_Pin B1_Pin R2_Pin
+                           G2_Pin B2_Pin */
+  GPIO_InitStruct.Pin = R1_Pin|G1_Pin|B1_Pin|R2_Pin
+                          |G2_Pin|B2_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
+
+  /*Configure GPIO pins : OE_Pin LAT_Pin */
+  GPIO_InitStruct.Pin = OE_Pin|LAT_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_PULLUP;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_HIGH;
+  HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
+
+  /* USER CODE BEGIN MX_GPIO_Init_2 */
+
+  /* USER CODE END MX_GPIO_Init_2 */
+}
+
+/* USER CODE BEGIN 4 */
+
+/* USER CODE END 4 */
+
+/**
+  * @brief  This function is executed in case of error occurrence.
+  * @retval None
+  */
+void Error_Handler(void)
+{
+  /* USER CODE BEGIN Error_Handler_Debug */
+  /* User can add his own implementation to report the HAL error return state */
+  __disable_irq();
+  while (1)
+  {
+  }
+  /* USER CODE END Error_Handler_Debug */
+}
+
+#ifdef  USE_FULL_ASSERT
+/**
+  * @brief  Reports the name of the source file and the source line number
+  *         where the assert_param error has occurred.
+  * @param  file: pointer to the source file name
+  * @param  line: assert_param error line source number
+  * @retval None
+  */
+void assert_failed(uint8_t *file, uint32_t line)
+{
+  /* USER CODE BEGIN 6 */
+  /* User can add his own implementation to report the file name and line number,
+     ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
+  /* USER CODE END 6 */
+}
+#endif /* USE_FULL_ASSERT */
---
+++ Core/Src/uart_protocol.c
@@ -0,0 +1,171 @@
+// uart_protocol.c
+
+#include "uart_protocol.h"
+#include "led_matrix.h"
+#include "led_font.h"
+#include "string.h"
+#include "stdio.h"
+
+
+extern UART_HandleTypeDef huart1;
+extern RTC_HandleTypeDef hrtc;
+extern uint8_t rx_byte;
+extern uint8_t led_enabled;
+
+uint8_t rx_byte = 0;
+uint16_t rx_index = 0;
+uint8_t rx_buffer[RX_BUFFER_SIZE] = {0};
+
+
+
+// Callback function triggered when a UART byte is received (Interrupt)
+void HAL_UART_RxCpltCallback(UART_HandleTypeDef *huart) {
+    if (huart->Instance == USART1) {
+        FSM_ParseByte(rx_byte);
+       HAL_UART_Receive_IT(&huart1, &rx_byte, 1);  // Continue receiving next byte
+    }
+}
+
+// Reverse the bit order of a value (used in CRC computation)
+unsigned short reverse_value(unsigned short value, int bit) {
+    unsigned short ret = 0;
+    for (int i = 0; i < bit; i++) {
+        ret = (ret << 1) | ((value >> i) & 1);
+    }
+    return ret;
+}
+
+// Calculate 16-bit CRC using polynomial 0x8005
+unsigned short crc16_calc(uint8_t *pData, int length) {
+    unsigned short crc = 0x0000;
+    while (length--) {
+        crc ^= reverse_value(*pData++, 8) << 8;
+        for (int i = 0; i < 8; i++) {
+            crc = (crc & 0x8000) ? (crc << 1) ^ 0x8005 : crc << 1;
+        }
+    }
+    crc = reverse_value(crc, 16);
+    return crc ^ 0x0000;
+}
+
+// Send a framed UART command with DLE-STX/ETX framing and CRC
+void UART_SendFrame(uint8_t cmd) {
+    uint8_t start[] = {DLE, STX};
+    uint8_t end[] = {DLE, ETX};
+    uint8_t frame[3]; // CMD + CRC(2 bytes)
+
+    frame[0] = cmd;
+    unsigned short crc = crc16_calc(&cmd, 1);
+    frame[1] = (crc >> 8) & 0xFF;
+    frame[2] = crc & 0xFF;
+
+    HAL_UART_Transmit(&huart1, start, 2, HAL_MAX_DELAY);
+
+    for (int i = 0; i < 3; i++) {
+        if (frame[i] == DLE) {
+            uint8_t dle_escape[] = {DLE, DLE};
+            HAL_UART_Transmit(&huart1, dle_escape, 2, HAL_MAX_DELAY);
+        } else {
+            HAL_UART_Transmit(&huart1, &frame[i], 1, HAL_MAX_DELAY);
+        }
+    }
+
+    HAL_UART_Transmit(&huart1, end, 2, HAL_MAX_DELAY);
+}
+
+// UART receive FSM for parsing framed protocol data byte-by-byte
+void FSM_ParseByte(uint8_t rx) {
+    static uint8_t dle_flag = 0;
+    static uint8_t receiving = 0;
+
+    if (!receiving) {
+        if (dle_flag && rx == STX) {
+            rx_index = 0;
+            receiving = 1;
+            dle_flag = 0;
+        } else if (rx == DLE) {
+            dle_flag = 1;
+        } else {
+            dle_flag = 0;
+        }
+    } else {
+        if (dle_flag) {
+            if (rx == ETX) {
+                ProcessCommand(rx_buffer, rx_index);
+                receiving = 0;
+                rx_index = 0;
+            } else if (rx == DLE) {
+                if (rx_index < RX_BUFFER_SIZE) rx_buffer[rx_index++] = DLE;
+            }
+            dle_flag = 0;
+        } else if (rx == DLE) {
+            dle_flag = 1;
+        } else {
+            if (rx_index < RX_BUFFER_SIZE) rx_buffer[rx_index++] = rx;
+        }
+    }
+}
+
+// Process a fully received command frame
+void ProcessCommand(uint8_t *data, uint16_t len) {
+    if (len < 4) return;
+
+    uint8_t dst_mask = data[0];
+    uint8_t cmd = data[1];
+
+    // payload: data[2] ~ data[len - 3]
+    uint16_t payload_len = len - 4;
+    uint8_t* payload = &data[2];
+
+    uint16_t recv_crc = (data[len - 2] << 8) | data[len - 1];
+    uint16_t calc_crc = crc16_calc(data, len - 2);
+    if (recv_crc != calc_crc) {
+        UART_SendFrame(CMD_NACK);
+        return;
+    }
+
+    if (((dst_mask >> (MY_ID - 1)) & 0x01) == 0) return;
+
+    switch (cmd) {
+        case CMD_LCD_ON:
+            led_enabled = 1;
+            HAL_GPIO_WritePin(LD2_GPIO_Port, LD2_Pin, GPIO_PIN_SET);
+            LEDMatrix_TurnOn();
+            UART_SendFrame(CMD_ACK);
+            break;
+
+        case CMD_LCD_OFF:
+            led_enabled = 0;
+            HAL_GPIO_WritePin(LD2_GPIO_Port, LD2_Pin, GPIO_PIN_RESET);
+            LEDMatrix_TurnOff();
+            UART_SendFrame(CMD_ACK);
+            break;
+
+        case CMD_SYNC_TIME:
+            if (payload_len == 7) {
+                RTC_TimeTypeDef newTime;
+                RTC_DateTypeDef newDate;
+
+                newDate.Year  = payload[0];
+                newDate.Month = payload[1];
+                newDate.Date  = payload[2];
+                newDate.WeekDay = RTC_WEEKDAY_MONDAY;
+
+                newTime.Hours   = payload[3];
+                newTime.Minutes = payload[4];
+                newTime.Seconds = payload[5];
+                newTime.TimeFormat = payload[6] ? RTC_HOURFORMAT12_PM : RTC_HOURFORMAT12_AM;
+                newTime.DayLightSaving = RTC_DAYLIGHTSAVING_NONE;
+                newTime.StoreOperation = RTC_STOREOPERATION_RESET;
+
+                HAL_RTC_SetTime(&hrtc, &newTime, RTC_FORMAT_BIN);
+                HAL_RTC_SetDate(&hrtc, &newDate, RTC_FORMAT_BIN);
+                UART_SendFrame(CMD_ACK);
+            }
+            break;
+
+        default:
+            UART_SendFrame(CMD_NACK);
+            break;
+    }
+}
