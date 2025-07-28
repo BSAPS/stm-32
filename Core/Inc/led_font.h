02c0bb0a6c90395283cc466e93402ddb4a1cb9ce
Jioh Jeon <69615755+gitgio99@users.noreply.github.com>
2025-07-28 18:49:10 +0900

    타이머 수정 및 gpio 설정 수정
    dma 관련한것 제거, gpio output 설정

---
+++ Core/Inc/led_font.h
@@ -0,0 +1,30 @@
+#ifndef __LED_FONT_H__
+#define __LED_FONT_H__
+
+#include <stdint.h>
+
+#define SMALL_FONT_WIDTH 7
+#define SMALL_FONT_HEIGHT 12
+
+#define BIG_FONT_WIDTH 11
+#define BIG_FONT_HEIGHT 16
+
+extern const uint8_t big_bitmap_0[BIG_FONT_HEIGHT][2];
+extern const uint8_t big_bitmap_1[BIG_FONT_HEIGHT][2];
+extern const uint8_t big_bitmap_2[BIG_FONT_HEIGHT][2];
+extern const uint8_t big_bitmap_3[BIG_FONT_HEIGHT][2];
+extern const uint8_t big_bitmap_4[BIG_FONT_HEIGHT][2];
+extern const uint8_t big_bitmap_5[BIG_FONT_HEIGHT][2];
+extern const uint8_t big_bitmap_6[BIG_FONT_HEIGHT][2];
+extern const uint8_t big_bitmap_7[BIG_FONT_HEIGHT][2];
+extern const uint8_t big_bitmap_8[BIG_FONT_HEIGHT][2];
+extern const uint8_t big_bitmap_9[BIG_FONT_HEIGHT][2];
+extern const uint8_t big_bitmap_COLON[BIG_FONT_HEIGHT][2];
+
+
+extern const uint8_t bitmap_A[12][1];
+extern const uint8_t bitmap_M[12][1];
+extern const uint8_t bitmap_cP[12][1];
+
+
+#endif
---
+++ Core/Inc/uart_protocol.h
@@ -0,0 +1,35 @@
+#ifndef UART_PROTOCOL_H
+#define UART_PROTOCOL_H
+
+#include "main.h"
+#include "stdint.h"
+
+// Protocol constants
+#define DLE        0x10
+#define STX        0x02
+#define ETX        0x03
+#define CMD_ACK    0xAA
+#define CMD_NACK   0x55
+#define CMD_LCD_ON  0x01
+#define CMD_LCD_OFF 0x02
+#define CMD_SYNC_TIME 0x03
+
+#define RX_BUFFER_SIZE 256
+#define CRC_INIT    0x0000
+#define CRC_POLY    0x8005
+#define IO_REF      1
+#define XOR_OUT     0x0000
+#define MY_ID       2
+extern uint8_t rx_byte;
+extern uint8_t rx_buffer[RX_BUFFER_SIZE];
+extern uint16_t rx_index;
+
+extern UART_HandleTypeDef huart1;
+
+// Functions
+void UART_SendFrame(uint8_t cmd);
+void FSM_ParseByte(uint8_t rx);
+void ProcessCommand(uint8_t *data, uint16_t len);
+unsigned short crc16_calc(uint8_t *pData, int length);
+
+#endif
